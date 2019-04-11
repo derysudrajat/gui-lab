69282aed2eb05800b72007d1ae7e878c0672f0fd
Dery Sudrajat <dery.sudrajat17@gmail.com>
2019-04-11 13:44:10 +0700

    Menambahkan File Praktikum 4

---
+++ Pertemuan4/FormSederhana/FormSederhana.py
@@ -0,0 +1,25 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+
+
+class FromSederhana(QWidget):
+    def __init__(self):
+        super(FromSederhana, self).__init__()
+
+        self.setupUi()
+
+    def setupUi(self):
+        self.resize(500, 200)
+        self.move(400, 400)
+        self.setWindowTitle('From Sederhana')
+
+        self.label = QLabel('Dosen Pengampu: ')
+        self.label.move(100, 40)
+        self.label.setParent(self)
+
+        self.label2 = QLabel('Afandi Nur Aziz Tohari, S.T., M.Cs')
+        self.label2.move(100, 60)
+        self.label2.setParent(self)
+
+        self.label3 = QLabel('Salin Kanan @2019')
+        self.label3.move(200, 180)
+        self.label3.setParent(self)
---
+++ Pertemuan4/FormSederhana/main.py
@@ -0,0 +1,12 @@
+import sys
+
+from Pertemuan4.FormSederhana.FormSederhana import*
+
+if __name__ == '__main__':
+    a = QApplication(sys.argv)
+    minfrom = FromSederhana()
+    minfrom.show()
+    a.exec_()
+
+
+
