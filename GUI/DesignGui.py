c46adf52fb3858567a6c5a738bc5ade9b49cc83b
derysudrajat <dery.sudrajat17@gmail.com>
2019-05-28 12:16:03 +0700

    Add DesignGui.py using form lessonfour.py

---
+++ GUI/DesignGui.py
@@ -0,0 +1,24 @@
+import sys
+
+from GUI.lessonfour import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+
+class MainForm(QDialog):
+    def __init__(self, parent=None):
+        QDialog.__init__(self, parent)
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+        self.ui.btnHello.clicked.connect(self.halloClicled)
+
+    def halloClicled(self):
+        QMessageBox.information(self, 'Demo Qt Designer',
+                                'Hallo %s, apa Kabar?' % self.ui.nameEdit.text())
+
+
+if __name__ == "__main__":
+    a = QApplication(sys.argv)
+    form = MainForm()
+    form.show()
+    a.exec_()
