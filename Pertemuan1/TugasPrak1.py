93e023dd9f41a960f1c5bde5b2886cad6caa0128
derysudrajat <dery.sudrajat17@gmail.com>
2019-06-13 13:47:31 +0700

    Add Pertemuan1

---
+++ Pertemuan1/TugasPrak1.py
@@ -0,0 +1,12 @@
+userInput = float(input("Masukan Nilai: "))
+print("Nilai Kamu: ")
+
+if userInput >= 80 : print("A")
+elif 80 > userInput >= 70: print("B")
+elif 70 > userInput >= 60: print("C")
+elif 60 > userInput >= 40: print("D")
+elif userInput < 40: print("E")
+
+
+
+
